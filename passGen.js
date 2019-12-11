11b325cafd863184084034a47a02ed7e6c251601
Crystal Ly <crystal.m.ly@gmail.com>
2019-12-10 23:04:00 -0600

    added javascript and added ids to textarea and onClick on buttons

--- passGen.js
+++ passGen.js
@@ -8,8 +8,25 @@ Can include special characters
 variables: each character will be randomly generated
 
 for loop will repeat 12 times to fill out random password.
-function will repeat the for loop when you click generate.
+function will repeat the for-loop when you click generate.
 copy button will copy the password text onto clipboard.
 
 */
 
+function generatePassword() {
+    var charList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@@#$%^&*()";
+    var pass = "";
+
+    for (var i = 0; i < 12; i++) {
+        pass += charList[Math.floor(Math.random() * charList.length)];
+    }
+    document.getElementById('passwordBox').value = pass;
+}
+
+function copyPassword() {
+    var copyText = document.getElementById("passwordBox");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+
+}
