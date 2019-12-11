7b672c5f984d338eba2dfa533bd434667c1014d7
Crystal Ly <crystal.m.ly@gmail.com>
2019-12-10 22:33:28 -0600

    added pseudocode and fixed some css styling

---
+++ passGen.js
@@ -0,0 +1,15 @@
+/* This is pseudocode for this password generator.
+Each time the script is run, a new password will be generated randomly.
+Password will be 12 characters long
+Can include A-Z and a-z (arrays?)
+Can include 0-9
+Can include special characters
+
+variables: each character will be randomly generated
+
+for loop will repeat 12 times to fill out random password.
+function will repeat the for loop when you click generate.
+copy button will copy the password text onto clipboard.
+
+*/
+
